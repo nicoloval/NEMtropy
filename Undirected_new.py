3ec940501e5cedb52b190bc27a6bbe3a6bdd94a7
EmilianoMarchese <emilianomarcheserc@gmail.com>
2020-08-05 10:50:22 +0200

    fixed exponential

--- Undirected_new.py
+++ Undirected_new.py
@@ -9,14 +9,15 @@ def iterative_cm_new(x, args):
     k = args[0]
     c = args[1]
     n = len(k)
+    x1 = np.exp(-x)
     f = np.zeros_like(k, dtype=np.float64)
     for i in np.arange(n):
         fx = 0
         for j in np.arange(n):
             if i == j:
-                fx += (c[j]-1) * (np.exp(-x[j])/(1+np.exp(-x[j])*np.exp(-x[i])))
+                fx += (c[j]-1) * (x1[j]/(1+x1[j]*x1[i]))
             else:
-                fx += (c[j]) * (np.exp(-x[j])/(1+np.exp(-x[j])*np.exp(-x[i])))
+                fx += (c[j]) * (x1[j]/(1+x1[j]*x1[i]))
         if fx:
             f[i] = -np.log(k[i]/fx)
 
@@ -28,14 +29,15 @@ def loglikelihood_cm_new(x, args):
     k = args[0]
     c = args[1]
     n = len(k)
+    x1 = np.exp(-x)
     f = 0.0
     for i in np.arange(n):
         f -= c[i] * k[i] * x[i]
         for j in np.arange(n):
             if i == j:
-                f -= (c[i]*(c[i]-1)*np.log(1+(np.exp(-x[i]))**2))/2
+                f -= (c[i]*(c[i]-1)*np.log(1+(x1[i])**2))/2
             else:
-                f -= (c[i]*c[j]*np.log(1+np.exp(-x[i])*np.exp(-x[j])))/2
+                f -= (c[i]*c[j]*np.log(1+x1[i]*x1[j]))/2
     return f
 
 
@@ -44,15 +46,16 @@ def loglikelihood_prime_cm_new(x, args):
     k = args[0]
     c = args[1]
     n = len(k)
+    x1 = np.exp(-x)
     f = np.zeros_like(k, dtype=np.float64)
     for i in np.arange(n):
         f[i] -= k[i]
         for j in np.arange(n):
             if i == j:
-                aux = np.exp(-x[i])**2
+                aux = x1[i]**2
                 f[i] += (c[i]-1) * (aux/(1+aux))
             else:
-                aux = np.exp(-x[i])* np.exp(-x[j])
+                aux = x1[i]* x1[j]
                 f[i] += c[j] * (aux/(1+aux))
     return f
 
@@ -62,6 +65,7 @@ def loglikelihood_hessian_cm_new(x, args):
     k = args[0]
     c = args[1]
     n = len(k)
+    x1 = np.exp(-x)
     f = np.zeros(shape=(n, n), dtype=np.float64)
     for i in np.arange(n):
         for j in np.arange(i, n):
@@ -69,13 +73,13 @@ def loglikelihood_hessian_cm_new(x, args):
                 aux_f = 0
                 for h in range(n):
                     if i == h:
-                        aux = np.exp(-x[h])**2
+                        aux = x1[h]**2
                         aux_f -= (aux/(1+aux)**2)*(c[h]-1)
                     else:
-                        aux = np.exp(-x[i])*np.exp(-x[h])
+                        aux = x1[i]*x1[h]
                         aux_f -= ((aux)/(1+aux)**2)*c[h]
             else:
-                aux = np.exp(-x[i])*np.exp(-x[j])
+                aux = x1[i]*x1[j]
                 aux_f = -((aux)/(1+aux)**2)*c[j]
 
             f[i, j] = aux_f
@@ -88,14 +92,15 @@ def loglikelihood_hessian_diag_cm_new(x, args):
     k = args[0]
     c = args[1]
     n = len(k)
+    x1 = np.exp(-x)
     f = np.zeros(n, dtype=np.float64)
     for i in np.arange(n):
         for j in np.arange(n):
             if i == j:
-                aux = np.exp(-x[j])**2
+                aux = x1[j]**2
                 f[i] -= (aux/(1+aux))*(c[j]-1)
             else:
-                aux = np.exp(-x[i])*np.exp(-x[j])
+                aux = x1[i]*x1[j]
                 f[i] -= (aux/(1+aux))*c[j]
     return f
 
