6e2b062b9d451262aac389e1fea2602027d492dc
EmilianoMarchese <emilianomarcheserc@gmail.com>
2020-08-05 11:15:56 +0200

    fixed exponential

--- Undirected_new.py
+++ Undirected_new.py
@@ -112,8 +112,8 @@ def iterative_ecm_new(sol,args):
 
     n = len(k)
 
-    x = sol[:n]
-    y = sol[n:]
+    x = np.exp(-sol[:n])
+    y = np.exp(-sol[n:])
 
     f = np.zeros(2*n, dtype=np.float64)
     for i in np.arange(n):
@@ -121,10 +121,10 @@ def iterative_ecm_new(sol,args):
         fy = 0.0
         for j in np.arange(n):
             if i!=j:
-                aux1 = np.exp(-x[i]) * np.exp(-x[j])
-                aux2 = np.exp(-y[i]) * np.exp(-y[j])
-                fx += ((np.exp(-x[j])*aux2)/(1-aux2+aux1*aux2))
-                fy += (aux1*np.exp(-y[j])/((1-aux2)*(1-aux2+aux1*aux2)))
+                aux1 = x[i] * x[j]
+                aux2 = y[i] * y[j]
+                fx += ((x[j]*aux2)/(1-aux2+aux1*aux2))
+                fy += ((aux1*y[j])/((1-aux2)*(1-aux2+aux1*aux2)))
         if fx:
             f[i] = -np.log(k[i] / fx)
         else:
@@ -143,14 +143,15 @@ def loglikelihood_ecm_new(sol,args):
 
     n = len(k)
 
-    x = sol[:n]
-    y = sol[n:]
+    x = np.exp(-sol[:n])
+    y = np.exp(-sol[n:])
+
     f = 0.0
     for i in np.arange(n):
-        f -= k[i] * x[i] + s[i] * y[i]
+        f -= k[i] * (-np.log(x[i])) + s[i] * (-np.log(y[i]))
         for j in np.arange(0,i):
-            aux1 = np.exp(-x[i]) * np.exp(-x[j])
-            aux2 = np.exp(-y[i]) * np.exp(-y[j])
+            aux1 = x[i] * x[j]
+            aux2 = y[i] * y[j]
             f -= np.log(1 + (aux1 * (aux2/(1-aux2))))
     return f
 
@@ -162,16 +163,17 @@ def loglikelihood_prime_ecm_new(sol,args):
 
     n = len(k)
 
-    x = sol[:n]
-    y = sol[n:]
+    x = np.exp(-sol[:n])
+    y = np.exp(-sol[n:])
+
     f = np.zeros(2*n,dtype = np.float64)
     for i in np.arange(n):
         f[i] -= k[i]
         f[i+n] -= s[i]
         for j in np.arange(n):
             if (i!=j):
-                aux1 = np.exp(-x[i])*np.exp(-x[j])
-                aux2 = np.exp(-y[i])*np.exp(-y[j])
+                aux1 = x[i]*x[j]
+                aux2 = y[i]*y[j]
                 f[i] += (aux1*aux2)/(1-aux2+aux1*aux2)
                 f[i+n] += (aux1*aux2)/((1-aux2)*(1-aux2+aux1*aux2))
     return f
@@ -184,8 +186,9 @@ def loglikelihood_hessian_ecm_new(sol,args):
 
     n = len(k)
 
-    x = sol[:n]
-    y = sol[n:]
+    x = np.exp(-sol[:n])
+    y = np.exp(-sol[n:])
+
     f = np.zeros(shape=(2*n,2*n),dtype=np.float64)
     for i in np.arange(n):
 
@@ -196,8 +199,8 @@ def loglikelihood_hessian_ecm_new(sol,args):
                 f3 = 0.0
                 for h in np.arange(n):
                     if h!=i:
-                        aux1 = np.exp(-x[i])*np.exp(-x[h])
-                        aux2 = np.exp(-y[i])*np.exp(-y[h])
+                        aux1 = x[i]*x[h]
+                        aux2 = y[i]*y[h]
                         aux3 = aux1*aux2
                         f1 -= ((aux3)*(1-aux2))/((1-aux2+aux3)**2)
                         f2 -= (aux3*(1-(aux2**2)+aux1*(aux2**2)))/(((1-aux2)**2)*((1-aux2+aux3)**2))
@@ -207,8 +210,8 @@ def loglikelihood_hessian_ecm_new(sol,args):
                 f[i+n,i] = f3
                 f[i,i+n] = f3
             else:
-                aux1 = np.exp(-x[i])*np.exp(-x[j])
-                aux2 = np.exp(-y[i])*np.exp(-y[j])
+                aux1 = x[i]*x[j]
+                aux2 = y[i]*y[j]
                 aux3 = aux1*aux2
                 aux4 = ((1-aux2+aux3)**2)
 
@@ -238,15 +241,16 @@ def loglikelihood_hessian_diag_ecm_new(sol,args):
 
     n = len(k)
 
-    x = sol[:n]
-    y = sol[n:]
+    x = np.exp(-sol[:n])
+    y = np.exp(-sol[n:])
+
     f = np.zeros(2*n,dtype=np.float64)
 
     for i in np.arange(n):
         for j in np.arange(n):
             if j!=i:
-                aux1 = np.exp(-x[i])*np.exp(-x[j])
-                aux2 = np.exp(-y[i])*np.exp(-y[j])
+                aux1 = x[i]*x[j]
+                aux2 = y[i]*y[j]
                 aux3 = aux1*aux2
                 f[i] -= ((aux3)*(1-aux2))/((1-aux2+aux3)**2)
                 f[i+n] -= (aux1*aux2*(1-(aux2**2)+aux1*(aux2**2)))/(((1-aux2)**2)*((1-aux2+aux3)**2))
