74d102d2e01b741a211d1b104e7f4a5324560054
EmilianoMarchese <emilianomarcheserc@gmail.com>
2020-09-28 15:11:17 +0200

    added new tests for undirected graphs

---
+++ tests/test_solve_ECM_quasinewton.py
@@ -0,0 +1,42 @@
+import sys
+import os
+sys.path.append('../')
+import Directed_graph_Class as sample
+import Undirected_graph_Class as sample_und
+import Matrix_Generator as mg
+import numpy as np
+import unittest
+
+
+class MyTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def test_ECM_Dianati_random_dense_20_undir(self):
+
+
+        network = mg.random_weighted_matrix_generator_dense(n=20, sup_ext = 10, sym=True, seed=None,
+                                                            intweights = True)
+        network_bin = (network>0).astype(int)
+    
+        g = sample_und.UndirectedGraph(adjacency=network)
+
+        g.solve_tool(model='ecm', method='quasinewton', max_steps=1000, verbose=False)
+
+        g.solution_error()
+
+        # test result
+        
+        self.assertTrue(g.error < 1e-1)
+        self.assertTrue(g.error < 1e-2)
+
+
+
+
+if __name__ == '__main__':
+    
+    unittest.main()
+    
