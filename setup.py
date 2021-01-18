cdf457f6a5e36d02f75bddd3be427affe77cc76e
EmilianoMarchese <emilianomarcheserc@gmail.com>
2021-01-18 12:56:33 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 with open('README.rst', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-#with open('HISTORY.rst', encoding='utf-8') as history_file:
-#    history = history_file.read()
+with open('HISTORY.rst', encoding='utf-8') as history_file:
+    history = history_file.read()
 
 
 setup(
@@ -17,6 +17,18 @@ setup(
     version="0.1.0",
     description="bla",
     license="GNU General Public License v3",
+    url = "https://github.com/nicoloval/classes/",
+    download_url = "https://github.com/nicoloval/classes/archive/master.zip",
+    keywords = ['Network reconstruction', 'Networks Null Models', 'Maximum Entrophy Methods'],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Network Science Community',      # Define that your audience are developers
+    'Topic :: ',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.8',
+                ],
     install_requires=["numpy==1.19.5",
                       "numba==0.52",
                       "networkx==2.5",
