947dff1f5eb987e12c7215c1b1e15be3bc1eb637
nicoval <nicolo.vallarano@imtlucca.it>
2021-01-27 00:03:27 +0100

    modified scripts name and removed empty files

--- src/netrecon/Undirected_new.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from numba import jit
-# Stops Numba Warning for experimental feature
-from numba.core.errors import NumbaExperimentalFeatureWarning
-import warnings
-
-warnings.simplefilter('ignore', category=NumbaExperimentalFeatureWarning)
-
