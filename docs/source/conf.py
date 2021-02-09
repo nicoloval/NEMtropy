1dfc1c1bca42c50bbeec77fc7397442f3e7aa1d0
EmilianoMarchese <emilianomarcheserc@gmail.com>
2021-02-09 11:46:51 +0100

    fixed bugs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-#sys.path.append('../src/')
+sys.path.append('../src/')
 
 
 # -- Project information -----------------------------------------------------
@@ -57,7 +57,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
