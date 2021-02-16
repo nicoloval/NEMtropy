bae85225e2ba0e5af13cc13ffe1b8ab9a66bfc1c
nicoval <nicolo.vallarano@imtlucca.it>
2021-02-16 11:20:33 +0100

    small fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Emiliano Marchese and Nicolo' Vallarano"
 author = "Emiliano Marchese, Nicolo' Vallarano"
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
