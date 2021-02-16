3b75ef3344cda7d232ca439e87c7631c6e8424d5
nicoval <nicolo.vallarano@imtlucca.it>
2021-02-16 10:56:58 +0100

    init upgraded

--- src/NEMtropy/__init__.py
+++ src/NEMtropy/__init__.py
@@ -3,6 +3,7 @@ from .matrix_generator import *
 from .ensemble_generator import *
 from .models_functions import *
 
-__version__ = "1.0.5"
+__version__ = "2.0.0"
 __author__ = """Nicolo' Vallarano (nicolo.vallarano@imtlucca.it)
-Emiliano Marchese (emiliano.marchese@imtulucca.it)"""
+Emiliano Marchese (emiliano.marchese@imtulucca.it)
+Matteo Bruno (matteo.bruno@imtlucca.it)"""
