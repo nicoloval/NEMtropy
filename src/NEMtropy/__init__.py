fc80aee6be301945ac42d4f6525494d01b378c2c
nicoval <nicolo.vallarano@imtlucca.it>
2021-02-26 16:34:09 +0100

    v2.0.1: updated version to 2.0.2 for next release

--- src/NEMtropy/__init__.py
+++ src/NEMtropy/__init__.py
@@ -3,7 +3,7 @@ from .matrix_generator import *
 from .ensemble_generator import *
 from .models_functions import *
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __author__ = """Nicolo' Vallarano (nicolo.vallarano@imtlucca.it)
 Emiliano Marchese (emiliano.marchese@imtulucca.it)
 Matteo Bruno (matteo.bruno@imtlucca.it)"""
